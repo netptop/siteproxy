d4b1ff7004b126fd5e3ff978fd2ac9fd2c3827a4
netptop <netptop@gmail.com>
2020-03-13 19:01:47 -0400

    fixed heroku crash issue.

--- logger.js
+++ logger.js
@@ -1,6 +1,9 @@
 let logStr = ''
 
 let logSave = (addStr) => {
+    if (typeof(addStr) !== 'string') {
+        return
+    }
     if (process.env.localFlag === 'true') {
         let timestr = new Date().toISOString()
         console.log(`${timestr}: ${addStr}`)
