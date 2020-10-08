bc9a1370e9bbed2f9ad8b3b98105e8cc0787edc8
majianlin <45617838+majianlin@users.noreply.github.com>
2020-10-08 20:58:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'aa-htr3efupu.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
