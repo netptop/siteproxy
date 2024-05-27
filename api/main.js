7ac999535cc46bac8b7b3122911e4a084ef71366
netptop <netptop@gmail.com>
2024-05-27 21:31:07 +0000

    v0.3.0 first version to display warning top bar for proxied website

---
+++ api/main.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+const ProxyMiddleware = require('http-proxy-middleware')
+var Proxy = require('../Proxy')
+
+let { blockedSites, urlModify, httpprefix, serverName, port, locationReplaceMap302, regReplaceMap, siteSpecificReplace, pathReplace } = require('../config')
+
+let cookieDomainRewrite = serverName
+
+let proxy = Proxy({ ProxyMiddleware, blockedSites, urlModify, httpprefix, serverName, port, cookieDomainRewrite, locationReplaceMap302, regReplaceMap, siteSpecificReplace, pathReplace })
+export default (req, res) => {
+  const dirPath = path.join(__dirname + '/..', req.url)
+  console.log(`x-forward-for:${req.headers['x-forwarded-for']}, req.url:${req.url}`)
+  if (req.url === '/' || req.url === '/index.html') {
+      let body = fs.readFileSync(path.join(__dirname, '../index.html'), 'utf-8')
+      res.status(200).send(body)
+      return
+  } else
+  if (fs.existsSync(dirPath) && !fs.lstatSync(dirPath).isDirectory()) {
+      let body = fs.readFileSync(dirPath)
+      return res.status(200).send(body)
+  }
+  proxy(req, res, null) // next: null
+}
